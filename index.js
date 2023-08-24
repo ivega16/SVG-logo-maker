329ef745941f7fa05fdcdbed6d0f7f2b283675f9
Isa Vega <imvega100@gmail.com>
2023-08-24 16:50:12 -0400

    fixed response on index.js

--- index.js
+++ index.js
@@ -1,23 +1,47 @@
 const Circle = require("./lib/Circle")
-const inquirer=require("inquirer")
-const fs=require("fs")
+const Square = require("./lib/Square")
+const Triangle = require("./lib/Triangle")
+const inquirer = require("inquirer")
+const fs = require("fs")
 
 inquirer
-.prompt([{
-    type: "input",
-    message: "Enter SVG text:",
-    name: "text"
-}, {
-    type: "list",
-    message: "Select text color:",
-    name: "textcolor",
-    choices: []
-}, {
-    type: "list",
-    message: "Choose a shape:",
-    name: "shape",
-    choices: ["Circle", "Triangle", "Square"]
-}])
-.then(response=> {
-
-})/
+    .prompt([{
+        type: "input",
+        message: "Enter text:",
+        name: "text"
+    },
+    {
+        type: "list",
+        message: "Select text color:",
+        name: "textColor",
+        choices: ["blue", "red", "white"]
+    },
+    {
+        type: "list",
+        message: "Choose a shape:",
+        name: "shape",
+        choices: ["circle", "triangle", "square"]
+    },
+    {
+        type: "list",
+        message: "Choose the shape's color:",
+        name: "shapeColor",
+        choices: ["purple", "green", "orange"]
+    }
+    ])
+    .then(response => {
+        if (response.shape === "circle") {
+            const circle = new Circle(response.text, response.textColor, response.shapeColor)
+            console.table(circle)
+            console.log(circle.render())
+            
+        } else if (response.shape === "square") {
+            const square = new Square(response.text, response.textColor, response.shapeColor)
+            console.table(square)
+            console.log(square.render())
+        } else {
+            const triangle = new Triangle(response.text, response.textColor, response.shapeColor)
+            console.table(triangle)
+            console.log(triangle.render())
+        }
+    })
